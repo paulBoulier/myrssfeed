d3dcd38f6abeead5bb0e313d73a301a96bd9d274
PaulBoulier <lhp6.pboulier@gmail.com>
2021-07-01 10:52:01 +0200

    modifications du fichier tableau

--- tableau.php
+++ tableau.php
@@ -13,28 +13,36 @@ $rssArray = [
 ];
 
 $categories = [];
+$rssArray_categories = [];
 
 foreach ($rssArray as $value) {
+    // valeur à incrémenter à chaque itération lorsque l'on parcours les items d'un flux (pour gérer les cas ou le flux est plus petit que 12 items)
     $nbItem = 0;
     $rssFlux = simplexml_load_file($value);
 
+    // le titre du flux que l'on parcours
     $substr = substr($rssFlux->channel->title, 0, -8);
+    // on push dans categories le titre du flux
     $categories[] = $substr;
+
     foreach ($rssFlux->channel->item as $value) {
         if ($nbItem < 12/*$_COOKIE["articleCount"]*/) {
+            // on découpe la valeur description du rss qui contient la description et l'image
             preg_match("/[^<]+(?=<)/", $value->description, $description);
             preg_match("/(?<=src=\").+(?=\")/", $value->description, $src);
-            array_push(
-                $itemArray,
-                [
-                    "cat" => $substr,
-                    "title" => (string) $value->title,
-                    "link" => trim((string) $value->link),
-                    "description" => trim($description[0]),
-                    "src" => $src[0],
-                    "date" => (string) $value->pubDate,
-                ]
-            );
+            // les clés que l'on aura dans notre array
+            $options = [
+                "cat" => $substr,
+                "title" => (string) $value->title,
+                "link" => trim((string) $value->link),
+                "description" => trim($description[0]),
+                "src" => $src[0],
+                "date" => (string) $value->pubDate,
+            ];
+            // on push dans l'array de façon désorganisée
+            array_push($itemArray, $options);
+            // on push dans l'array par rapport au nom de la catégorie
+            $rssArray_categories[$substr] = $options;
         } else {
             break;
         }
