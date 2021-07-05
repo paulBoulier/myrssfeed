a4c3dc521240fddede5d7dd58e988083d9146990
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-07-05 10:20:16 +0200

    changement de la navbar

--- views/parameters.php
+++ views/parameters.php
@@ -11,6 +11,7 @@ $selectedSubjects = !empty($_COOKIE) && null !== ($selected = $_COOKIE["selected
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/style/style.css">
     <title>Document</title>
 </head>
